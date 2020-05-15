77385c68935a8027c77de744ece984e5405d3d0d
Liversticks <55716846+Liversticks@users.noreply.github.com>
2020-05-14 22:44:54 -0700

    skeleton dynamic HTML layout

--- testWR/records/views.py
+++ testWR/records/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from records.models import Run
+
+import sqlite3
+import os
 
 # Create your views here.
+def index(request):
+	
+	num_categories = Run.objects.all().count()
+	num_valid_categories = Run.objects.exclude(playerName="").count()
+	# list containing all distinct entries in the "game" column
+	# Manually collected for now
+	myDB = sqlite3.connect('./db.sqlite3')
+	# see StackOverflow "sqlite3 fetchall to list"
+	myDB.row_factory = lambda cursor, row: row[0]
+	list_of_games = myDB.execute("SELECT DISTINCT game FROM records_run").fetchall()
+	myDB.close()
+		
+	# query database to get data
+	valid_runs = Run.objects.exclude(playerName="")
+	
+	# dictionary linking keys to values derived above
+	context = {
+		'num_categories': num_categories,
+		'num_valid_categories': num_valid_categories,
+		'list_of_games': list_of_games,
+		'valid_runs': valid_runs
+	}
+	
+	return render(request, 'index.html', context=context)
